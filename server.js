77ccf5e043ddfd3c68350751e97cc00756010000
ItsGodHere <shouro-agoro@et.esiea.fr>
2021-09-27 13:28:29 +0200

    restucturation et server fonctionnel

--- server.js
+++ server.js
@@ -3,14 +3,23 @@ var serve_static = require('serve-static');
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
+var utils = require('./public/utils');
 
 console.log('Chargement de la base de donnée...')
-let database = JSON.parse(fs.readFileSync('nameDataBase.json'));
+let database = JSON.parse(fs.readFileSync('./public/nameDataBase.json'));
 
 var app = express();
 
 //Activation du serveur statique
 app.use(serve_static(__dirname + "/public"))
 
+app.get('/generate/name', function (req, res)
+{
+    var entier = utils.randomNumber(0, 99);
+    res.send(database[entier]["name"])
+})
+
 var serveur = http.Server(app);
-serveur.listen(8080, function(){});
+serveur.listen(8080, function () {
+    console.log('Serveur en ecoute sur le port 8080...')
+});
